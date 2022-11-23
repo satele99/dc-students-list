818a51827cb9f8f462ae0b430e1ce0974662f995
Amir Ali <satele.works@gmail.com>
2022-11-23 13:39:23 -0500

    updated the styling and boilerplate

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const addStudentButton = document.getElementById('student-submit');
+// let names = document.getElementById('student-input').value;
+
+addStudentButton.addEventListener('click', event => {
+    console.log('hello');
+});
